4894c0aed68b047304f6364a794fb01047be8d5c
craftbrak <louisdewilde2001@gmail.com>
2023-12-03 15:51:04 +0100

    phase 2 complete

--- chalange1/src/main.rs
+++ chalange1/src/main.rs
@@ -6,7 +6,7 @@ use std::num::ParseIntError;
 fn main() {
     println!("Hello, world!");
     let string = read_file("./input.txt").unwrap();
-    let strings: Vec<&str> = string.split("\n").collect();
+    let strings: Vec<&str> = string.lines().collect();
     let mut codes: Vec<i32>= Vec::new();
     for s in strings {
         match extract_code(s) {
@@ -32,13 +32,36 @@ fn read_file(file_path: &str)-> io::Result<String>{
 fn extract_code(source: &str)->Result<i32, Option<ParseIntError>>{
     let mut first_digit:Option<char>= None;
     let mut last_digit: Option<char>= None;
-    for c in source.chars() {
-        if c.is_numeric() {
-            if first_digit.is_none() {
-               first_digit = Some(c);
-            }
-            last_digit = Some(c);
+    let digit_pairs = [
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
+    let mut word_star_index:usize = 0;
+    for (i, c) in source.char_indices() {
+
+        let digit=is_digit_or_digit_word(&source,&c,0,i+1);
+        match digit {
+            None => {}
+            Some(d) => {first_digit= Some(d);break}
         }
+    }
+    for (i, c) in source.char_indices().rev() {
+
+        let digit=is_digit_or_digit_word(&source,&c,i,source.len());
+        match digit {
+            None => {}
+            Some(d) => {last_digit= Some(d);break}
+        }
+    }
+    if first_digit.is_some() {
+        println!("digit founds for this line {}: {}{} ",source,first_digit.unwrap(),last_digit.unwrap());
     }
    return  match (first_digit, last_digit) {
         (Some(c1), Some(c2)) => {
@@ -49,4 +72,29 @@ fn extract_code(source: &str)->Result<i32, Option<ParseIntError>>{
             }
         }
         _ =>{Err(None)}}
+}
+fn is_digit_or_digit_word(s:&str, c:&char, i_start:usize, i_end:usize)->Option<char>{
+    let digit_pairs = [
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
+    if c.is_digit(10){
+        return Some(*c);
+    }
+    else {
+        let word= &s[i_start..i_end];
+        for &(name, d) in &digit_pairs {
+            if s[i_start..i_end].contains(name) {
+                return Some(d);
+            }
+        }
+    }
+    None
 }
